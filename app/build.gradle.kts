092054cea25c4abb08ab180eff5af1b1b1239b91
acbikram <8.8821295e+07+acbikram@users.noreply.github.com>
2026-08-18 20:18:42 +0000

    Release post-baseline appearance onboarding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.industrial.barcodescanner"
         minSdk = 29
         targetSdk = 34
-        versionCode = 15
-        versionName = "2.0.4"
+        versionCode = 16
+        versionName = "2.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
