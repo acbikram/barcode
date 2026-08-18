eedd05897ad1a3dc0ce0a546eb38e2f9f65063e1
acbikram <8.8821295e+07+acbikram@users.noreply.github.com>
2026-08-18 21:36:09 +0000

    Release v2.0.7 build 18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.industrial.barcodescanner"
         minSdk = 29
         targetSdk = 34
-        versionCode = 17
-        versionName = "2.0.6"
+        versionCode = 18
+        versionName = "2.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
